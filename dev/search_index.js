f7e027bdbbf5c663442bcd3f51725a7a34a82b0b
Documenter.jl <documenter@juliadocs.github.io>
2026-02-06 03:01:06 +0000

    build based on 1c25dad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyPkg19.jl","page":"Home","title":"MyPkg19.jl","text":"Documentation for MyPkg19.jl.","category":"section"},{"location":"#Quick-Start","page":"Home","title":"Quick Start","text":"import Pkg; Pkg.add(\"MyPkg19\")\nimport MyPkg19; MyPkg19.hello()","category":"section"},{"location":"#API-reference","page":"Home","title":"API reference","text":"","category":"section"},{"location":"#MyPkg19.hello-Tuple{}","page":"Home","title":"MyPkg19.hello","text":"hello() -> String\n\n\n\n\n\n\n","category":"method"}]
+}
